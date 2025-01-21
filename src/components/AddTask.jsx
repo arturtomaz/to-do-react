1ff8adf3f91bc1608d410f7fa8006a526caaab1c
artur tomaz <arturtomaz1@gmail.com>
2025-01-21 10:36:31 -0300

    All English

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -8,28 +8,26 @@ function AddTask({ addTask }) {
     <div className="space-y-4 p-6 bg-slate-200 rounded-md shadow flex flex-col">
       <Input
         type="text"
-        placeholder="Digite o título da tarefa"
+        placeholder="Enter the task title"
         value={title}
         onChange={(event) => setTitle(event.target.value)}
       />
       <Input
         type="text"
-        placeholder="Digite a descrição da tarefa"
+        placeholder="Enter the task description"
         value={description}
         onChange={(event) => setDescription(event.target.value)}
       />
       <button
-        onClick={() => {  
-            if(!title.trim() || !description.trim()) {
-                return alert('Preencha o título e a descrição da tarefa.')
-            }
-            addTask(title, description),
-            setTitle(''),
-            setDescription('')
+        onClick={() => {
+          if (!title.trim() || !description.trim()) {
+            return alert("Preencha o título e a descrição da tarefa.");
+          }
+          addTask(title, description), setTitle(""), setDescription("");
         }}
         className="bg-slate-500 text-white px-4 py-2 rounded-md font-medium"
       >
-        Adicionar
+        Add task
       </button>
     </div>
   );
